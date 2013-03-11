3713fa6719302c45d902b4e0c263154868a558ef
jiangwei zhao <zhaojiangwei6464@163.com>
2013-03-11 21:08:53 +0800

    fix

--- src/core/firstblood.c
+++ src/core/firstblood.c
@@ -3,9 +3,11 @@
 #include <firstblood.h>
 
 int main(int argc, char *argv[]){
-	#ifdef _LINUX_CONFIG_H_INCLUDED
+	#ifdef _LINUX_CONFIG_H_INCLUDED_
 		printf("%d\n", 1);
 	#else
 		printf("%d\n", 2);
 	#endif
+
+	return 0;
 }
