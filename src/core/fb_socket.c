89723b134e7ac7b1aff9ffe3afb67248db3ea391
jiangwei zhao <zhaojiangwei6464@163.com>
2013-03-17 13:06:31 +0800

    fix

--- src/core/fb_socket.c
+++ src/core/fb_socket.c
@@ -1,15 +1,18 @@
 #include <fb_core.h>
 #include <fb_config.h>
 
+static void sig_child(int signo);
+
 int fb_open_socket(){
-	int listen_fd, i;
+	int listen_fd, i, conn_fd;
+	pid_t child_pid;
 	fb_sockaddr_in sockaddr;
 	
 	if((listen_fd = fb_socket(AF_INET, SOCK_STREAM, 0)) == -1) return _FB_ERROR_;
 	i = 1;
 	if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (void*) &i, sizeof(i) ) < 0) return _FB_ERROR_;
 
-	memset(&sockaddr, '0', sizeof(sockaddr));
+	memset(&sockaddr, 0, sizeof(sockaddr));
 	
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_port = htons(_FB_LISTENED_FD_);
@@ -17,26 +20,63 @@ int fb_open_socket(){
 
 	if(bind(listen_fd, (struct sockaddr *) &sockaddr, sizeof(sockaddr)) == -1) return _FB_ERROR_;
 	if(listen(listen_fd, 10) == -1) return _FB_ERROR_;
+
+	if(signal(SIGCHLD, sig_child) == SIG_ERR){
+		printf("SIGCHLD error\n");
+		return -1;
+	}
+
 	while(1){
-		fb_connect(listen_fd);
-		sleep(1);
+		conn_fd = accept(listen_fd, (struct sockaddr *) NULL, NULL);
+		if(conn_fd < 0){
+			printf("connect error\n");
+			break;
+		}
+
+		if((child_pid = fork()) < 0){
+			printf("fork error\n");
+			break;
+		}else if(child_pid == 0){
+			/*child process*/
+			if(close(listen_fd) < 0){
+				printf("child close listen_fd error\n");
+			}
+
+			fb_connect(conn_fd);
+			if(close(conn_fd) < 0){
+				printf("child close conn_fd error\n");
+			}
+			exit(0);
+		}
+
+		if(close(conn_fd) < 0){
+			printf("parent close conn_fd error\n");
+		}
 	}
 
 	close(listen_fd);	
 	return 0;
 }
 
-void fb_connect(int listen_fd){
+void fb_connect(int conn_fd){
 	fb_http_header_t http_header;
-	int conn_fd;
 
 	memset(&http_header, '\0', sizeof(http_header));
-	conn_fd = accept(listen_fd, (struct sockaddr *) NULL, NULL);
 	fcntl(conn_fd, F_SETFL, O_NONBLOCK);
 
 	fb_get_http_request(conn_fd, &http_header);
 	if(close(conn_fd) < 0){
 		printf("close conn_fd error\n");
 		exit(-1);
+	}
+}
+
+static void sig_child(int signo){
+	if(signo != SIGCHLD){
+		printf("catch error\n");
+	}else{
+		if(waitpid(-1, NULL, WNOHANG) <= 0){
+			printf("child exit error\n");
+		}
 	}
 }
